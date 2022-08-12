586e4521620e77d5ecaec8079090cd76063fca94
Samuel-ACH <samuelantoniocastro2021@gmail.com>
2022-08-12 17:13:24 -0600

    Codigo del Cargar y actualizar Banco

--- JS/Banco.js
+++ JS/Banco.js
@@ -1,4 +1,8 @@
 var UrlBancos = 'http://20.216.41.245:90/G8_20/controller/Banco.php?opc=GetBancos';
+var UrlInsertBancos = 'http://20.216.41.245:90/G8_20/controller/Banco.php?opc=InsertBanco';
+var UrlGetBanco = 'http://20.216.41.245:90/G8_20/controller/Banco.php?opc=GetBanco';
+var UrlUpdateBanco = 'http://20.216.41.245:90/G8_20/controller/Banco.php?opc=UpdateBanco';
+
 
 $(document).ready(function(){
     CargarBancos();
@@ -14,7 +18,7 @@ function CargarBancos(){
             var Valores = '';
 
             for(i=0; i< MiItems.length; i++){
-                Valores +=  '<tr>'+
+                Valores += '<tr>'+
                 '<td>' + MiItems[i].codigo_banco + '</td>' +
                 '<td>' + MiItems[i].nombre_banco + '</td>' +
                 '<td>' + MiItems[i].oficina_principal + '</td>' +
@@ -22,9 +26,99 @@ function CargarBancos(){
                 '<td>' + MiItems[i].pais + '</td>' +
                 '<td>' + MiItems[i].fechafundacion + '</td>' +
                 '<td>' + MiItems[i].RTN + '</td>' +
+                '<td>'+
+                '<button class="btn btn-info" onclick="CargarBanco('+ MiItems[i].codigo_banco +')">Editar</button>'+
+                '</td>'+
             '</tr>';
             $('#DataBancos').html(Valores);
             }
         }
     });
 }
+
+function AgregarBancos(){
+    var datosbancos = {
+        codigo_banco:$('#codigo_banco').val(),
+        nombre_banco:$('#nombre_banco').val(),
+        oficina_principal:$('#oficina_principal').val(),
+        cantidad_sucursales:$('#cantidad_sucursales').val(),
+        pais:$('#pais').val(),
+        fechafundacion:$('#fechafundacion').val(),
+        RTN:$('#RTN').val(), 
+    };
+    var datosbancosjson= JSON.stringify(datosbancos);
+
+    $.ajax({
+        url: UrlInsertBancos,
+        type:'POST',
+        data:datosbancosjson,
+        datatype:'JSON',
+        contentType:'application/json',
+        success:function(reponse){
+            console.log(reponse);
+            alert('Banco Agregado correctamente'); 
+        },
+        error: function(textStatus, errorThrown){
+            alert('Error al ingresar el banco'+ textStatus + errorThrown);  
+        }
+    });
+    alert('Aviso');
+}
+function CargarBanco(CodigoBanco){
+    var datosbancos = {
+        codigo_banco: CodigoBanco
+    };
+    var datosbancosjson = JSON.stringify(datosbancos);
+
+    $.ajax({
+        url: UrlGetBanco,
+        type: 'POST',
+        data: datosbancosjson,
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function (response) {
+            var MiItems = response;
+            $('#codigo_banco').val(MiItems[0].codigo_banco);
+            $('#nombre_banco').val(MiItems[0].nombre_banco);
+            $('#oficina_principal').val(MiItems[0].oficina_principal);
+            $('#cantidad_sucursales').val(MiItems[0].cantidad_sucursales);
+            $('#pais').val(MiItems[0].pais);
+            $('#fechafundacion').val(MiItems[0].fechafundacion);
+            $('#RTN').val(MiItems[0].RTN);
+           var btnactualizar = '<input type="submit" id="btn-actualizar" onclick="ActualizarBanco('+ MiItems[0].codigo_banco + ')"'+
+        'value="Actualizar Banco" class="btn btn-primary"></input>';
+        $('#btnagregarbanco').html(btnactualizar);
+        }
+    });
+}
+
+function ActualizarBanco(CodigoBanco){
+    var datosbancos = {
+        codigo_banco: CodigoBanco,
+        codigo_banco:$('#codigo_banco').val(),
+        nombre_banco:$('#nombre_banco').val(),
+        oficina_principal:$('#oficina_principal').val(),
+        cantidad_sucursales:$('#cantidad_sucursales').val(),
+        pais:$('#pais').val(),
+        fechafundacion:$('#fechafundacion').val(),
+        RTN:$('#RTN').val(), 
+    };
+    var datosbancosjson = JSON.stringify(datosbancos);
+
+    $.ajax({
+        url:UrlUpdateBanco,
+        type:'PUT',
+        data:datosbancosjson,
+        datatype:'JSON',
+        contentType:'application/json',
+        success:function (reponse){
+            console.log(reponse);
+            alert("Error al actualizar");
+        },
+        error: function(textStatus, errorThrown){
+            alert('Se actualizo con exito'+ textStatus);
+        }
+    });
+    alert('Aviso');
+}
+
